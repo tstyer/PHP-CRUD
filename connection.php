bc6c02f4c5c284801b008a87e071559616b492d8
tstyer <travisstyer.ts@gmail.com>
2026-03-22 19:30:43 +0000

    Confirm connection in connection.php

---
+++ connection.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db_name = "crud";
+
+$conn = new mysqli($servername, $username, $password, $db_name);
+
+// check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connected successfully";
+?>
